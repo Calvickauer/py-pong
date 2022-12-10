060817a2da0aefd34506fc620a8a57387b5c0184
calvickauer <calvickauer@gmail.com>
2022-12-09 22:15:10 -0800

    score display and ball reset added

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame 
 
+pygame.font.init()
+
 WIDTH, HEIGHT = 700, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 
@@ -12,6 +14,8 @@ BLACK = (0, 0, 0)
 
 PADDLE_HEIGHT, PADDLE_WIDTH = 100, 20
 BALL_RADIUS = 7
+
+SCORE_FONT = pygame.font.SysFont("comicsans", 50)
 
 class Paddle:
     COLOR = WHITE
@@ -37,8 +41,8 @@ class Ball:
     COLOR = WHITE
     
     def __init__(self, x, y, radius):
-        self.x = x
-        self.y = y
+        self.x = self.original_x =  x
+        self.y = self.original_y = y
         self.radius = radius
         self.x_vel = self.MAX_VEL
         self.y_vel = 0
@@ -50,9 +54,20 @@ class Ball:
         self.x += self.x_vel
         self.y += self.y_vel
 
-def draw(win, paddles, ball):
+    def reset(self):
+        self.x = self.original_x
+        self.y = self.original_y
+        self.y_vel = 0
+        self.x_vel *= -1
+        
+
+def draw(win, paddles, ball, l_score, r_score):
     win.fill(BLACK)
     
+    left_score_text = SCORE_FONT.render(f"{l_score}", 1, WHITE)
+    right_score_text = SCORE_FONT.render(f"{r_score}", 1, WHITE)
+    win.blit(left_score_text, (WIDTH//4 - left_score_text.get_width()//2, 20))
+    win.blit(right_score_text, (WIDTH * (3/4) - right_score_text.get_width()//2, 20))
     for paddle in paddles:
         paddle.draw(win)
     
@@ -112,9 +127,12 @@ def main():
     right_paddle = Paddle(WIDTH - 10 - PADDLE_WIDTH, HEIGHT//2 - PADDLE_HEIGHT//2, PADDLE_WIDTH, PADDLE_HEIGHT)
     ball = Ball(WIDTH//2, HEIGHT//2, BALL_RADIUS)
     
+    l_score = 0
+    r_score= 0
+    
     while run:
         clock.tick(FPS)
-        draw(WIN, [left_paddle, right_paddle], ball)
+        draw(WIN, [left_paddle, right_paddle], ball, l_score, r_score)
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -125,6 +143,13 @@ def main():
         
         ball.move()
         handle_collision(ball, left_paddle, right_paddle)
+        
+        if ball.x < 0:
+            r_score += 1
+            ball.reset()
+        elif ball.x > WIDTH:
+            l_score += 1
+            ball.reset()
     
     pygame.quit()
 
